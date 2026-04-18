783a0bef802315296f254a04749c45b36bef9357
Husna <ghodkehusna95@gmail.com>
2026-04-18 14:57:17 +0530

    Added role management module

--- backened/app.py
+++ backened/app.py
@@ -27,59 +27,50 @@ def login():
 
 
 # In-memory data
+# In-memory data
 departments = [
     {"id": 1, "name": "HR", "description": "Human Resource", "status": True},
     {"id": 2, "name": "IT", "description": "Tech Department", "status": True}
 ]
 
-# Get all departments
-@app.route('/all-departments', methods=['GET'])
-def all_departments():
-    return jsonify(departments)
+# ---------------- ROLE MANAGEMENT ---------------- #
 
-# Add department
-@app.route('/add-department', methods=['POST'])
-def add_department():
-    data = request.json
+roles = [
+    {"id": 1, "name": "Admin", "description": "Full Access", "status": True},
+    {"id": 2, "name": "Manager", "description": "Manage Team", "status": True}
+]
+# Get all roles
+@app.route('/all-roles', methods=['GET'])
+def all_roles():
+    return jsonify(roles)
 
-    new_dept = {
-        "id": len(departments) + 1,
-        "name": data['name'],
-        "description": data['description'],
+# Add role
+@app.route('/add-role', methods=['POST'])
+def add_role():
+    data = request.get_json()
+
+    new_role = {
+        "id": len(roles) + 1,
+        "name": data.get("name"),
+        "description": data.get("description"),
         "status": True
     }
 
-    departments.append(new_dept)
-    return jsonify({"message": "Department added"})
+    roles.append(new_role)
+    return jsonify({"message": "Role added"})
 
-# Soft delete
-@app.route('/delete-department/<int:id>', methods=['PUT'])
-def delete_department(id):
-    for d in departments:
-        if d['id'] == id:
-            d['status'] = False
-    return jsonify({"message": "Deleted"})
+# Delete role (soft delete)
+@app.route('/delete-role/<int:id>', methods=['PUT'])
+def delete_role(id):
+    for r in roles:
+        if r['id'] == id:
+            r['status'] = False
+    return jsonify({"message": "Role deleted"})
 
-# Restore
-@app.route('/restore-department/<int:id>', methods=['PUT'])
-def restore_department(id):
-    for d in departments:
-        if d['id'] == id:
-            d['status'] = True
-    return jsonify({"message": "Restored"})
-
-@app.route('/update-department/<int:id>', methods=['PUT'])
-def update_department(id):
-    data = request.get_json()
-
-    for d in departments:
-        if d['id'] == id:
-            d['name'] = data.get("name")
-            d['description'] = data.get("description")
-
-    return jsonify({"message": "Updated"})
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
-
-    # Login API
+# Restore role
+@app.route('/restore-role/<int:id>', methods=['PUT'])
+def restore_role(id):
+    for r in roles:
+        if r['id'] == id:
+            r['status'] = True
+    return jsonify({"message": "Role restored"})
